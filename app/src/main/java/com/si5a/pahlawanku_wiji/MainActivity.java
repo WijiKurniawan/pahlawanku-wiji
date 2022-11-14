3fc05dfee21bd1f7dcc04b76239e93ba3f1c7b86
WijiKurniawan <wijikurniawan121@gmail.com>
2022-11-14 14:16:15 +0700

    first-commit

---
+++ app/src/main/java/com/si5a/pahlawanku_wiji/MainActivity.java
@@ -0,0 +1,14 @@
+package com.si5a.pahlawanku_wiji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
