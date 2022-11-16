e4e1f89e74cef53e57c1db9f38301f2e7fd3fc2c
WijiKurniawan <wijikurniawan121@gmail.com>
2022-11-16 16:58:22 +0700

    memperbaiki main activity

--- app/src/main/java/com/si5a/pahlawanku_wiji/ExampleInstrumentedTest.java
+++ app/src/main/java/com/si5a/pahlawanku_wiji/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.si5a.pahlawanku_wiji", appContext.getPackageName());
+        Assert.assertEquals("com.si5a.pahlawanku_wiji", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/si5a/pahlawanku_wiji/MainActivity.java
+++ app/src/main/java/com/si5a/pahlawanku_wiji/MainActivity.java
@@ -1,14 +1,37 @@
 package com.si5a.pahlawanku_wiji;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvPahlawan;
+    private ArrayList<ModelPahlawan> data = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvPahlawan = findViewById(R.id.rv_pahlawan);
+        rvPahlawan.setHasFixedSize(true);
+
+        data.addAll(DataPahlawan.ambilDataPahlawan());
+        tampilDataCard();
     }
+    private void tampilDataCard() {
+        rvPahlawan.setLayoutManager(new LinearLayoutManager(this));
+        AdapterCard AC = new AdapterCard(data);
+        rvPahlawan.setAdapter(AC);
+    }
+
+
+
+
+}
 }
